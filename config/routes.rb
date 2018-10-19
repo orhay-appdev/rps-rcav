6b5a39b9bf40a06b42accd57a7ac5e0c10441226
Or Hay <ohay@chicagobooth.edu>
2018-10-19 23:28:45 +0000

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+  get("/rock", {:controller => "game", :action => "user_plays_rock"})
+  get("/paper", {:controller => "game", :action => "user_plays_paper"})
+  get("/scissors", {:controller => "game", :action => "user_plays_scissors"})
+  get("/", {:controller => "game", :action => "index"})
+  
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
